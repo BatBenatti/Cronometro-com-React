6f56e1d451bd74e933dbe9236cd612bd271d02c2
unknown <lmbenattti@gmail.com>
2023-07-13 07:51:49 -0300

    Adicionando prpriedade de seleção para o item clickado

--- src/Components/Lista/Item/index.tsx
+++ src/Components/Lista/Item/index.tsx
@@ -1,15 +1,31 @@
 import { ITarefa } from "../../../types/tarefa";
 import style from "../Lista.module.scss";
 
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecioanda: ITarefa) => void;
+}
+
 export default function Item({
   tarefa,
   tempo,
   selecionado,
   completado,
   id,
-}: ITarefa) {
+  selecionaTarefa,
+}: Props) {
   return (
-    <li className={style.item}>
+    <li
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ""}`}
+      onClick={() =>
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id,
+        })
+      }
+    >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
     </li>
